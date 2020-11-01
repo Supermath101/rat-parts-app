24405a357f7828511383748d41981a3a7799e2ae
Matthew Hirsch <hirschmatthew9@gmail.com>
2020-10-31 20:56:30 -0400

    Added basic part list

--- src/routes/parts/index.js
+++ src/routes/parts/index.js
@@ -1,16 +1,24 @@
 import { h } from 'preact';
 import {useEffect, useState} from "preact/hooks";
 import style from './style.css';
+import data from "../../data/data"
+import { Link } from 'preact-router/match';
 
 // Note: `parts` comes from the URL, courtesy of our router
 const Parts = ({ part }) => {
 	
 	return (
-		<div class={style.parts}>
+        <div class={style.parts}>
 			<h1>
-				{part ? `Part: ${part}` : "Part List"}
+				{part ? `Part: ${data.parts[part].name}` : "Part List"}
 			</h1>
-			{/* TODO: add specific part information and general part list. */}
+			<div>
+				<ul class={style.part_list}>
+				{data.parts.map((part, i) => <li>
+						<Link class={style.part_link} href={`/parts/${i}`}>{part.name}</Link>
+					</li>)}
+				</ul>
+			</div>
 		</div>
 	);
 }
